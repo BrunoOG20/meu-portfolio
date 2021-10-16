56e91009202f3d3d61072c136f31ba0bc8f518a1
BrunoOG20 <brunogava2@gmail.com>
2021-10-16 02:09:51 -0300

    Criacao do Home hero

---
+++ src/components/Experiencias/index.tsx
@@ -0,0 +1,36 @@
+import SectionTitle from '../SectionTitle';
+import ExperienciaItem from './ExperienciaItem';
+import { Container } from './styles';
+
+function Experiencias() {
+  return (
+    <Container>
+      <SectionTitle title="05 Anos" description="de experiência" />
+
+      <section>
+        <ExperienciaItem
+          year="2021"
+          title="Dev web"
+          description="Lorem ipsum dolor sdsadas it amet, consectetur adipiscing elit. Aliquam"
+        />
+        <ExperienciaItem
+          year="2021"
+          title="Dev web"
+          description="Lorem ipsum sss dolor sit amet, consectetur adipiscing elit. Aliquam"
+        />
+        <ExperienciaItem
+          year="2021"
+          title="Dev web"
+          description="Lorem idsadas psum dolor sit amet, consectetur adipiscing elit. Aliquam"
+        />
+        <ExperienciaItem
+          year="2020"
+          title="Dev web"
+          description="Lorem ipsumdsada  dolor sit amet, consectetur adipiscing elit. Aliquam"
+        />
+      </section>
+    </Container>
+  );
+}
+
+export default Experiencias;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,58 @@
-import Header from "../components/Header";
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import { useEffect } from 'react';
+import Aos from 'aos';
+import Head from 'next/head';
+import { HomeContainer } from '../styles/HomeStyles';
 
-export default function Home() {
+import Header from '../components/Header';
+import HomeHero from '../components/HomeHero';
+
+import 'aos/dist/aos.css';
+import Experiencias from '../components/Experiencias';
+
+interface IProjeto {
+  slug: string;
+  title: string;
+  type: string;
+  description: string;
+  link: string;
+  thumbnail: string;
+}
+
+interface HomeProps {
+  projetos: IProjeto[];
+}
+
+export default function Home({ projetos }: HomeProps) {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+
   return (
-    <Header />
+    <HomeContainer>
+      <Head>
+        <title>Home | Meu portfólio</title>
+        <meta
+          name="description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:image:secure_url" content="/ogimage.png" />
+        <meta name="twitter:image" content="/ogimage.png" />
+        <meta name="twitter:image:src" content="/ogimage.png" />
+        <meta
+          property="og:description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+      </Head>
+
+      <Header />
+
+      <main className="container">
+        <HomeHero />
+        <Experiencias />
+      </main>
+    </HomeContainer>
   );
 }
